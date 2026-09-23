55d9a95a4bb2cafad2f6f6498dba98c15ae8933c
AKEEM ADETUNJI <akeemadetunji1234@gmail.com>
2026-09-23 21:45:04 +0100

    Fix use server: replace export * with explicit named re-exports

--- src/app/actions/admin.ts
+++ src/app/actions/admin.ts
@@ -1,5 +1,42 @@
 "use server";
 
-export * from "./adminPart1";
-export * from "./adminPart2a";
-export * from "./adminPart2b";
+// Next.js requires explicit named exports of async functions from "use server" files.
+// Do not use export * here.
+
+export {
+  getDashboardStats,
+  getOperationalDiagnostics,
+  getAdminNotificationReport,
+  getAgents,
+  getUnverifiedAgents,
+  verifyAgent,
+  activateAgent,
+  deactivateAgent,
+  rejectAgent,
+  getAllUsers,
+  getCorpMembers,
+  getPremiumPayments,
+  getPayouts,
+} from "./adminPart1";
+
+export {
+  updateUserRole,
+  toggleUserBan,
+  deleteUserAccount,
+  upgradeToPremium,
+  revokePremium,
+  getAdminAnalytics,
+  getRegionalHeatmapData,
+} from "./adminPart2a";
+
+export {
+  getArtisans,
+  createArtisan,
+  updateArtisan,
+  deleteArtisan,
+  verifyArtisan,
+  getAgentFraudRiskReport,
+  getPendingProperties,
+  updatePropertyStatus,
+  getPartners,
+} from "./adminPart2b";
